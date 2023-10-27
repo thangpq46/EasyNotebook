7e6eeadb6267097a0da526e723bbea5feb51acd7
pqt46 <pqt4621@gmail.com>
2023-10-27 13:26:53 +0700

    add menu

--- app/src/main/java/com/qt46/easynotebook/notes/MainActivity.kt
+++ app/src/main/java/com/qt46/easynotebook/notes/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 }) { paddingValues ->
+
                     Surface(
                         modifier = Modifier
                             .fillMaxSize()
